714255affa9ed88242ede370bcc6e9e03ef2787c
“Mathéo <matheo.deghaye@gmail.com>
2024-04-17 09:21:23 +0200

    code robot modifié

--- code/Mining_simulator/src/main/java/code/Robot.java
+++ code/Mining_simulator/src/main/java/code/Robot.java
@@ -6,50 +6,65 @@ import java.util.*;
  */
 public class Robot {
 
-    /**
-     * Default constructor
-     */
+    private int id_rob;
+    private int storage;
+    private int efficiency;
+    private int x;
+    private int y;
+    private int xw;
+    private int yw;
+    private int inventory;
+
     public Robot() {
+        this.id_rob = 0;
+        this.storage = new Random().nextInt(5)+4;
+        this.efficiency = new Random().nextInt(2)+1;
+        this.x = 0;
+        this.y = 0;
+        this.xw = 0;
+        this.yw = 0;
+        this.inventory = 0;
     }
 
-    /**
-     * 
-     */
-    private int id_rob;
+    public Robot(int id_rob, int x, int y, int xw, int yw, int inventory) {
+        this.id_rob = id_rob;
+        this.x = x;
+        this.y = y;
+        this.xw = xw;
+        this.yw = yw;
+        this.storage = new Random().nextInt(5)+4;
+        this.efficiency = new Random().nextInt(2)+1;
+        this.inventory = inventory;
+    }
 
-    /**
-     * 
-     */
-    private int storage;
+    public int getId_rob() {
+        return id_rob;
+    }
 
-    /**
-     * 
-     */
-    private int efficiency;
+    public int getStorage() {
+        return storage;
+    }
 
-    /**
-     * 
-     */
-    private int x;
+    public int getEfficiency() {
+        return efficiency;
+    }
 
-    /**
-     * 
-     */
-    private int y;
+    public int getX() {
+        return x;
+    }
 
-    /**
-     * 
-     */
-    private int xw;
+    public int getY() {
+        return y;
+    }
 
-    /**
-     * 
-     */
-    private int yw;
+    public int getXw() {
+        return xw;
+    }
 
-    /**
-     * 
-     */
+    public int getYw() {
+        return yw;
+    }
+
     public void mine() {
         // TODO implement here
     }
