cb71c5e286f72f89a5d8568057843a86c2c277fe
Nyasaoto <lohann.allostry@gmail.com>
2024-04-18 17:48:37 +0200

    ajout arraylist warehouses et mines

--- code/Mining_simulator/src/main/java/code/World.java
+++ code/Mining_simulator/src/main/java/code/World.java
@@ -5,8 +5,11 @@ public class World {
     private Section[][] sections = new Section[10][10];
     private ArrayList<Robot> robots_gold = new ArrayList<Robot>();
     private ArrayList<Robot> robots_nickel = new ArrayList<Robot>();
+    private ArrayList<Warehouse> warehouses = new ArrayList<Warehouse>();
+    private ArrayList<Mine> mines = new ArrayList<Mine>();
 
     public World() {
+
     }
 
     public Section get_section(int x, int y) {
