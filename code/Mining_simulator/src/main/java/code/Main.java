d08d987612663d4791ddb8a81ab060cff66fc098
adrien <nekaisqd@gmail.com>
2024-04-19 12:33:21 +0200

    main + tab toString World

--- code/Mining_simulator/src/main/java/code/Main.java
+++ code/Mining_simulator/src/main/java/code/Main.java
@@ -1,11 +1,27 @@
 package code;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
         World world = new World();
         while (true) {
             System.out.println(world);
-            
+            for (Robot robot : world.get_robot()) {
+                System.out.println(robot);
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("1. Move | 2. Mine | 3. Exit");
+                int choice = scanner.nextInt();
+                if (choice == 1) {
+                    System.out.println("N. North | S. South | O. West | E. Est");
+                    String direction = scanner.next();
+                    robot.move(direction);
+                } else if (choice == 2) {
+                    robot.mine();
+                } else if (choice == 3) {
+                    System.exit(0);
+                }
+            }
         }
     }
 }
