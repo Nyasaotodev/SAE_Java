693bebf8a12145b58f63f44e93ca50d89fc96b1a
adrien <nekaisqd@gmail.com>
2024-04-19 12:10:07 +0200

    rename world World remove Warehouse

---
+++ code/Mining_simulator/src/main/java/code/Main.java
@@ -0,0 +1,11 @@
+package code;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        World world = new World();
+        while (true) {
+            System.out.println(world);
+            
+        }
+    }
+}
--- code/Mining_simulator/src/main/java/code/World.java
+++ code/Mining_simulator/src/main/java/code/World.java
@@ -2,7 +2,7 @@ package code;
 import java.util.*;
 public class World {
 
-    private Section[][] sections = new Section[10][10];
+    private Section[][] world = new Section[10][10];
     private ArrayList<Robot> robots_gold = new ArrayList<Robot>();
     private ArrayList<Robot> robots_nickel = new ArrayList<Robot>();
     private ArrayList<Warehouse> warehouses = new ArrayList<Warehouse>();
@@ -19,22 +19,22 @@ public class World {
 
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
-                this.sections[i][j] = new Section(i, j);
+                this.world[i][j] = new Section(i, j);
             }
         }
 
         while (water > 0) {
             int x = new Random().nextInt(10);
             int y = new Random().nextInt(10);
-            int chance = 20;
+            int chance = 10;
             ArrayList<int[]> arounds = around(x, y);
             for (int[] around : arounds) {
-                if (this.sections[around[0]][around[1]].get_water()) {
+                if (this.world[around[0]][around[1]].get_water()) {
                     chance += 20;
                 }
             }
             if (new Random().nextInt(100) < chance) {
-                this.sections[x][y].set_water(true);
+                this.world[x][y].set_water(true);
                 water--;
             }
         }
@@ -42,66 +42,66 @@ public class World {
         while (gold_mine > 0) {
             int x = new Random().nextInt(10);
             int y = new Random().nextInt(10);
-            if (this.sections[x][y].get_water() || this.sections[x][y].get_struct() != null) {
+            if (this.world[x][y].get_water() || this.world[x][y].get_struct() != null) {
                 continue;
             }
             id++;
-            this.sections[x][y].set_struct(new Mine(0, x, y, ore.gold));
-            this.mines.add((Mine)this.sections[x][y].get_struct());
+            this.world[x][y].set_struct(new Mine(0, x, y, ore.gold));
+            this.mines.add((Mine)this.world[x][y].get_struct());
             gold_mine--;
         }
         while (nickel_mine > 0) {
             int x = new Random().nextInt(10);
             int y = new Random().nextInt(10);
-            if (this.sections[x][y].get_water() || this.sections[x][y].get_struct() != null) {
+            if (this.world[x][y].get_water() || this.world[x][y].get_struct() != null) {
                 continue;
             }
             id++;
-            this.sections[x][y].set_struct(new Mine(id, x, y, ore.nickel));
-            this.mines.add((Mine)this.sections[x][y].get_struct());
+            this.world[x][y].set_struct(new Mine(id, x, y, ore.nickel));
+            this.mines.add((Mine)this.world[x][y].get_struct());
             nickel_mine--;
         }
         id = 0;
         while (ware_gold > 0) {
             int x = new Random().nextInt(10);
             int y = new Random().nextInt(10);
-            if (this.sections[x][y].get_water() || this.sections[x][y].get_struct() != null) {
+            if (this.world[x][y].get_water() || this.world[x][y].get_struct() != null) {
                 continue;
             }
-            this.sections[x][y].set_struct(new Warehouse(0, x, y, ore.gold));
-            this.warehouses.add((Warehouse)this.sections[x][y].get_struct());
+            this.world[x][y].set_struct(new Warehouse(0, x, y, ore.gold));
+            this.warehouses.add((Warehouse)this.world[x][y].get_struct());
             ware_gold--;
 
             while (gold_robots > 0) {
                 int x_rob = new Random().nextInt(10);
                 int y_rob = new Random().nextInt(10);
-                if (this.sections[x_rob][y_rob].get_water() || this.sections[x_rob][y_rob].get_struct() != null) {
+                if (this.world[x_rob][y_rob].get_water() || this.world[x_rob][y_rob].get_struct() != null) {
                     continue;
                 }
                 id++;
-                this.sections[x_rob][y_rob].set_robot(new Robot(id, x_rob, y_rob, x, y, 0, this, ore.gold));
-                this.robots_gold.add(this.sections[x_rob][y_rob].get_robot());
+                this.world[x_rob][y_rob].set_robot(new Robot(id, x_rob, y_rob, x, y, 0, this, ore.gold));
+                this.robots_gold.add(this.world[x_rob][y_rob].get_robot());
                 gold_robots--;
             }
         }
         while (ware_nickel > 0) {
             int x = new Random().nextInt(10);
             int y = new Random().nextInt(10);
-            if (this.sections[x][y].get_water() || this.sections[x][y].get_struct() != null) {
+            if (this.world[x][y].get_water() || this.world[x][y].get_struct() != null) {
                 continue;
             }
-            this.sections[x][y].set_struct(new Warehouse(1, x, y, ore.nickel));
-            this.warehouses.add((Warehouse)this.sections[x][y].get_struct());
+            this.world[x][y].set_struct(new Warehouse(1, x, y, ore.nickel));
+            this.warehouses.add((Warehouse)this.world[x][y].get_struct());
             ware_nickel--;
             while (nickel_robots > 0) {
                 int x_rob = new Random().nextInt(10);
                 int y_rob = new Random().nextInt(10);
-                if (this.sections[x_rob][y_rob].get_water() || this.sections[x_rob][y_rob].get_struct() != null) {
+                if (this.world[x_rob][y_rob].get_water() || this.world[x_rob][y_rob].get_struct() != null) {
                     continue;
                 }
                 id++;
-                this.sections[x_rob][y_rob].set_robot(new Robot(id, x_rob, y_rob, x, y, 0, this, ore.nickel));
-                this.robots_nickel.add(this.sections[x_rob][y_rob].get_robot());
+                this.world[x_rob][y_rob].set_robot(new Robot(id, x_rob, y_rob, x, y, 0, this, ore.nickel));
+                this.robots_nickel.add(this.world[x_rob][y_rob].get_robot());
                 nickel_robots--;
             }
         }
@@ -126,7 +126,7 @@ public class World {
     }
 
     public Section get_section(int x, int y) throws out_of_bound_exception {
-        return this.sections[x][y];
+        return this.world[x][y];
     }
     public String toString() {
         String result = "";
@@ -135,27 +135,27 @@ public class World {
         sep += "+\n";
         result += sep;
         for (int i = 0; i < 10; i++) {
+            result += "|";
             for (int j = 0; j < 10; j++) {
-                result += "|";
-                if (this.sections[i][j].get_struct() != null) {
-                    if (this.sections[i][j].get_struct() instanceof Mine) {
-                        result += "M " + this.sections[i][j].get_struct().get_id() + "|";
+                if (this.world[i][j].get_struct() != null) {
+                    if (this.world[i][j].get_struct() instanceof Mine) {
+                        result += "M " + this.world[i][j].get_struct().get_id() + "|";
                     } else {
-                        result += "W " + this.sections[i][j].get_struct().get_id() + "|";
+                        result += "W " + this.world[i][j].get_struct().get_id() + "|";
                     }
-                } else if (this.sections[i][j].get_water()) {
-                    result += "W W|";
+                } else if (this.world[i][j].get_water()) {
+                    result += "X X|";
                 } else {
                     result += "   |";
                 }
             }
             result += "\n";
+            result += "|";
             for (int j = 0; j < 10; j++) {
-                result += "+";
-                if (this.sections[i][j].get_robot() != null) {
-                    result += "R " + this.sections[i][j].get_robot().get_id() + "|";
-                } else if (this.sections[i][j].get_water()) {
-                    result += "W W|";
+                if (this.world[i][j].get_robot() != null) {
+                    result += "R " + this.world[i][j].get_robot().get_id() + "|";
+                } else if (this.world[i][j].get_water()) {
+                    result += "X X|";
                 } else {
                     result += "   |";
                 }
